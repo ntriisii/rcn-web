2ef684b7493484cc2fcc07bb4f4756b67a3ad03a
ntriissii <ntriissii@gmail.com>
2026-03-11 15:13:22 +0200

    refactor(emacs): cleanup remaining local viewers and re-export required functions for YAML context

--- rcn_web/viewers/emacs/__init__.py
+++ rcn_web/viewers/emacs/__init__.py
@@ -1,6 +1,6 @@
 from .url import *
 from .target import *
-from .match_groups import *
+from pentest_utils.viewers.emacs.match_groups import *
 from .nuclei_vulns import *
 from .fuzzing import *
 from .flows import *
